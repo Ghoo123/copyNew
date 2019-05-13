d3ea0e0eb76a8e165e3392edc33fb12eeb774e0f
Ghoo123 <50377294+Ghoo123@users.noreply.github.com>
2019-05-13 13:14:55 +0500

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -21,16 +21,18 @@ router.post('/clientcrm', function(req, res, next) {
   opts.fileNamePattern = 'request.txt';
   // const log = require('simple-node-logger').createRollingFileLogger( opts );
 
-  apiData.lastname = req.body.lastName;
-  apiData.firstname = req.body.firstName;
+  apiData.lastName = req.body.lastName;
+  apiData.firstName = req.body.firstName;
   apiData.email = req.body.email;
-  apiData.telephone = req.body.phone;
-  apiData.countryiso = req.body.countryCode;
-  apiData.apikey = {affiliateId:'51363aa0ca7743f2af06a45308e87baa'};
+  apiData.phone = req.body.phone;
+  apiData.affiliateInfo = {affiliateId:'24066'};
+  apiData.countryCode = req.body.countryCode;
+  apiData.password  = req.body.password;
   console.log(apiData);
   // log.info('subscription to ',apiData, ' accepted at ', new Date().toJSON());
   var options = { method: 'POST',
-  url: 'https://api.water-pipeline.com/SignalsServer/api/registerUser',
+  url: 'https://api.finte.co/v3/users',
+  qs: { webId: '08cd854f-d1b2-4250-90ab-c0bf827749fc' },
   headers: 
    { 'Content-Type': 'application/json' },
   body: JSON.stringify(apiData) };
@@ -41,6 +43,45 @@ request(options, function (error, response,crmData) {
     res.json(crmData);
 });
 
+
+});
+
+
+
+router.post('/affliates', function(req, res, next) {
+  let apiData = {}
+    console.log('in client crm body',req.body)
+
+  opts.logDirectory = './public/logFiles/';
+  opts.fileNamePattern = 'request.txt';
+  // const log = require('simple-node-logger').createRollingFileLogger( opts );
+
+  apiData.lastName = req.body.lastName;
+  apiData.firstName = req.body.firstName;
+  apiData.email = req.body.email;
+  apiData.phone = req.body.phone;
+  apiData.countryCode = req.body.countryCode;
+  apiData.password  = req.body.password;
+  console.log(apiData);
+  var options = { method: 'POST',
+  url: 'https://api.water-pipeline.com/SignalsServer/api/registerUser',
+  qs: 
+   { firstname: apiData.firstName,
+     lastname: apiData.lastName,
+     email: apiData.email,
+     telephone: apiData.phone,
+     countryiso: apiData.countryCode,
+     apikey: '51363aa0ca7743f2af06a45308e87baa' },
+  headers: 
+   { 'Postman-Token': '62b16b75-d54d-48bf-865c-7d93f3c7539d',
+     'cache-control': 'no-cache' } };
+
+request(options, function (error, response, body) {
+  if (error)
+    res.json(error);
+    else
+    res.json(body);
+});
 
 });
 module.exports = router;
