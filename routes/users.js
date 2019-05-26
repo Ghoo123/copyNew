136744662796146187720ba303ec3a42b4b75591
Ghoo123 <50377294+Ghoo123@users.noreply.github.com>
2019-05-26 14:39:06 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,53 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
+const fixieRequest = request.defaults({ 'proxy': ' http://fixie:T0RpNcgPkdIzKLi@olympic.usefixie.com:80'});
 const opts = {
-  errorEventName:'error',
-      logDirectory:'', // NOTE: folder must exist and be writable...
-      fileNamePattern:'',
-      dateFormat:'YYYY.MM.DD'
+    errorEventName: 'error',
+    logDirectory: '', // NOTE: folder must exist and be writable...
+    fileNamePattern: '',
+    dateFormat: 'YYYY.MM.DD'
 };
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 
-router.post('/affliates', function(req, res, next) {
-  let apiData = {}
-    console.log('in client crm body',req.body)
+router.post('/affliates', function (req, res, next) {
+    let apiData = {}
+    console.log('in client crm body', req.body)
 
-  opts.logDirectory = './public/logFiles/';
-  opts.fileNamePattern = 'request.txt';
-  // const log = require('simple-node-logger').createRollingFileLogger( opts );
+    opts.logDirectory = './public/logFiles/';
+    opts.fileNamePattern = 'request.txt';
+    // const log = require('simple-node-logger').createRollingFileLogger( opts );
 
-  apiData.lastName = req.body.lastName;
-  apiData.firstName = req.body.firstName;
-  apiData.email = req.body.email;
-  apiData.phone = req.body.phone;
-  apiData.countryCode = req.body.countryCode;
-  apiData.password  = req.body.password;
-  console.log(apiData);
-  var options = { method: 'POST',
-  url: 'https://api.water-pipeline.com/SignalsServer/api/registerUser',
-  qs: 
-   { firstname: apiData.firstName,
-     lastname: apiData.lastName,
-     email: apiData.email,
-     telephone: apiData.phone,
-     countryiso: apiData.countryCode,
-     apikey: '51363aa0ca7743f2af06a45308e87baa' },
-  headers: 
-   { 'Postman-Token': '62b16b75-d54d-48bf-865c-7d93f3c7539d',
-     'cache-control': 'no-cache' } };
+    apiData.lastName = req.body.lastName;
+    apiData.firstName = req.body.firstName;
+    apiData.email = req.body.email;
+    apiData.phone = req.body.phone;
+    apiData.countryCode = req.body.countryCode;
+    apiData.password = req.body.password;
+    console.log(apiData);
+    var options = {
+        method: 'POST',
+        url: 'https://api.water-pipeline.com/SignalsServer/api/registerUser',
+        qs:
+         {
+             firstname: apiData.firstName,
+             lastname: apiData.lastName,
+             email: apiData.email,
+             telephone: apiData.phone,
+             countryiso: apiData.countryCode,
+             apikey: '51363aa0ca7743f2af06a45308e87baa'
+         },
+        headers:
+         {
+             'Postman-Token': '62b16b75-d54d-48bf-865c-7d93f3c7539d',
+             'cache-control': 'no-cache'
+         }
+    };
 
-request(options, function (error, response, body) {
-  if (error)
-    res.json(error);
-    else
-    res.json(body);
-});
+    fixieRequest(options, function (err, data, body) {
+      res.json(body);
+    });
 
 });
 module.exports = router;
+// Your code here!
